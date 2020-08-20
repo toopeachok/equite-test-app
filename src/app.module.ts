848da23aabbd11f0c4b41a026e164b4d69907a22
Toopeachok <2peachok@gmail.com>
2020-08-21 00:41:50 +0300

    Added logging and error handling

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,23 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DataModule } from './data/data.module';
+import { HttpExceptionFilter } from './shared/http-exception.filter';
+import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
+import { LoggingInterceptor } from './shared/logging.interceptor';
 
 @Module({
   imports: [DataModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_FILTER,
+      useClass: HttpExceptionFilter,
+    },
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: LoggingInterceptor,
+    },
+  ],
 })
 export class AppModule {}
---
+++ src/shared/http-exception.filter.ts
@@ -0,0 +1,38 @@
+import {
+  ExceptionFilter,
+  Catch,
+  ArgumentsHost,
+  HttpException,
+  HttpStatus,
+  Logger,
+} from '@nestjs/common';
+import { Request, Response } from 'express';
+
+@Catch()
+export class HttpExceptionFilter implements ExceptionFilter {
+  catch(exception: HttpException, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+    const request = ctx.getRequest<Request>();
+    const status = exception.getStatus();
+
+    const errorResponse = {
+      statusCode: status,
+      timestamp: new Date().toISOString(),
+      path: request.url,
+      method: request.method,
+      message:
+        status !== HttpStatus.INTERNAL_SERVER_ERROR
+          ? exception.message || null
+          : 'Internal server error',
+    };
+
+    Logger.error(
+      `${request.method} ${request.url}`,
+      JSON.stringify(errorResponse),
+      'ExceptionFilter',
+    );
+
+    response.status(status).json(errorResponse);
+  }
+}
