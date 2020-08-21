4ecc52dc66504a8c03bed79cc1fe732e4a6b3e55
Toopeachok <2peachok@gmail.com>
2020-08-21 22:53:39 +0300

    Added content-type to response headers

--- src/data/data.controller.ts
+++ src/data/data.controller.ts
@@ -3,6 +3,24 @@ import { Request, Response } from 'express';
 
 import { DataService } from './data.service';
 
+const mimeType = {
+  '.ico': 'image/x-icon',
+  '.html': 'text/html',
+  '.js': 'text/javascript',
+  '.json': 'application/json',
+  '.css': 'text/css',
+  '.png': 'image/png',
+  '.jpg': 'image/jpeg',
+  '.wav': 'audio/wav',
+  '.mp3': 'audio/mpeg',
+  '.svg': 'image/svg+xml',
+  '.pdf': 'application/pdf',
+  '.zip': 'application/zip',
+  '.doc': 'application/msword',
+  '.eot': 'application/vnd.ms-fontobject',
+  '.ttf': 'application/x-font-ttf',
+};
+
 @Controller('data')
 export class DataController {
   constructor(private dataService: DataService) {}
@@ -14,9 +32,12 @@ export class DataController {
 
   @Get(':filename')
   getFileData(@Req() request: Request, @Res() res: Response) {
+    const ext = this.dataService.getFileExt(request.url);
+
     this.dataService
       .getFileData(request.url)
       .then(data => {
+        res.setHeader('Content-type', mimeType[ext] || 'text/plain');
         res.end(data);
       })
       .catch(err => {
--- src/data/data.service.ts
+++ src/data/data.service.ts
@@ -1,6 +1,5 @@
 import { HttpException, HttpStatus, Injectable, Logger } from '@nestjs/common';
 
-import * as url from 'url';
 import * as path from 'path';
 import * as fs from 'fs';
 import * as util from 'util';
@@ -13,23 +12,27 @@ export class DataService {
     return 'Please specify file name. For example: http://localhost:4000/data/file.txt';
   }
 
-  async getFileData(fileName: string): Promise<Buffer> {
-    const parsedFileName = path
-      .normalize(url.parse(fileName).pathname)
-      .replace(/^(\.\.[\/\\])+/, '');
-
-    const filePath = path.join(__dirname, '..\\..\\', parsedFileName);
-    Logger.log(filePath, 'GetFileData');
-
-    const ext = path.parse(parsedFileName).ext;
+  getFileExt(fileURL: string): string {
+    const parsedFileURL = path.normalize(fileURL).replace(/^(\.\.[\/\\])+/, '');
+    const filePath = path.join(__dirname, '..\\..\\', parsedFileURL);
+    const ext = path.parse(filePath).ext;
 
     if (ext === '') {
       throw new HttpException(
         'Please specify file extension',
         HttpStatus.BAD_REQUEST,
       );
-    } else {
-      return readFile(filePath);
     }
+
+    return ext;
+  }
+
+  async getFileData(fileURL: string): Promise<Buffer> {
+    const parsedFileURL = path.normalize(fileURL).replace(/^(\.\.[\/\\])+/, '');
+
+    const filePath = path.join(__dirname, '..\\..\\', parsedFileURL);
+    Logger.log(filePath, 'FilePath');
+
+    return readFile(filePath);
   }
 }
